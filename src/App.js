9592c120915e8f76e24a638a2d8ab085776a57d5
esma <esma.ea.aksoy@gmail.com>
2023-12-06 15:02:38 +0300

    icons

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import Doktors from "./component/Doktors";
 import Appoinment from "./component/Appoinment";
 import {appointmentData} from "./helper/data";
 import {doctorData} from "./helper/data";
+import { FaUserDoctor } from "react-icons/fa6";
 function App() {
   return (
     <>
       <header>
-      <h1>HOSPITAL</h1>
+      <h1>HOSPITAL <FaUserDoctor className="text-primary"/></h1>
     
       </header>
       <main>
